b512fb2cee249f2dcfe0f5488a02fc44eca604ff
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-23 18:06:53 +0530

    Added : Computation for daily wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,9 +1,11 @@
 import math
 import random
 IS_PRESENT = 1
+WAGE_PER_HOUR = 20
+FULL_DAY_HOURS = 8 
 
 checkemp = random.randint(0,1)
 if checkemp == IS_PRESENT:
-    print("Employee is present")
+    print(f"Employee is present and the wage is {WAGE_PER_HOUR * FULL_DAY_HOURS}rs")
 else:
     print("Employee is absent")
