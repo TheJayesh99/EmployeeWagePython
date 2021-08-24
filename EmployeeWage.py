b17f782df2a57ed01b11150b26a9627109a1dc31
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-24 10:36:22 +0530

    Added : Method to compute employee wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -9,19 +9,22 @@ PART_TIME_HOURS = 4
 TOTAL_WORKING_DAYS = 20 
 TOTAL_WORKING_HOURS = 100
 
-empAttendence = {
-    IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
-    IS_PRESENT_PART_TIME: PART_TIME_HOURS,
-    IS_ABSENT: 0
-    }
+def calculateEmployeeWage():
+    empAttendence = {
+        IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
+        IS_PRESENT_PART_TIME: PART_TIME_HOURS,
+        IS_ABSENT: 0
+        }
 
-workingDays = 0
-totalWorkingHours = 0
+    workingDays = 0
+    totalWorkingHours = 0
 
-while(workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
-    checkEmp = random.randint(0,2)
-    totalWorkingHours = totalWorkingHours + empAttendence.get(checkEmp)
-    workingDays = workingDays + 1
+    while(workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
+        checkEmp = random.randint(0,2)
+        totalWorkingHours = totalWorkingHours + empAttendence.get(checkEmp)
+        workingDays = workingDays + 1
 
-totalWage = totalWorkingHours * WAGE_PER_HOUR
+    return totalWorkingHours * WAGE_PER_HOUR
+
+totalWage = calculateEmployeeWage() 
 print("Employee total wage is ",totalWage)
