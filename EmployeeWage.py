0da0ae08c000ddc6ebe1a179a6ed33308b359bbd
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-24 10:22:37 +0530

    Added : Calculate wage till max working hours or working days is reached

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,4 +1,5 @@
 import random
+
 IS_ABSENT = 0
 IS_PRESENT_FULL_DAY = 1
 IS_PRESENT_PART_TIME = 2
@@ -6,16 +7,21 @@ WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8 
 PART_TIME_HOURS = 4
 TOTAL_WORKING_DAYS = 20 
+TOTAL_WORKING_HOURS = 100
 
 empAttendence = {
-    IS_PRESENT_FULL_DAY : WAGE_PER_HOUR * FULL_DAY_HOURS,
-    IS_PRESENT_PART_TIME: WAGE_PER_HOUR * PART_TIME_HOURS,
+    IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
+    IS_PRESENT_PART_TIME: PART_TIME_HOURS,
     IS_ABSENT: 0
     }
 
-totalWage = 0
-for i in range(TOTAL_WORKING_DAYS):
-    checkEmp = random.randint(0,2)
-    totalWage = totalWage + empAttendence.get(checkEmp)
+workingDays = 0
+totalWorkingHours = 0
 
+while(workingDays < TOTAL_WORKING_DAYS and totalWorkingHours < TOTAL_WORKING_HOURS):
+    checkEmp = random.randint(0,2)
+    totalWorkingHours = totalWorkingHours + empAttendence.get(checkEmp)
+    workingDays = workingDays + 1
+
+totalWage = totalWorkingHours * WAGE_PER_HOUR
 print("Employee total wage is ",totalWage)
