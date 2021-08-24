4fe1e0138ceedf304063b4ead274b64e7e55a6fa
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-24 09:54:37 +0530

    Added : Dictionary to get employee daily wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,15 +1,19 @@
-import math
 import random
+IS_ABSENT = 0
 IS_PRESENT_FULL_DAY = 1
 IS_PRESENT_PART_TIME = 2
 WAGE_PER_HOUR = 20
 FULL_DAY_HOURS = 8 
 PART_TIME_HOURS = 4
 
-checkemp = random.randint(0,2)
-if checkemp == IS_PRESENT_FULL_DAY:
-    print(f"Employee is present for full time and the wage is {WAGE_PER_HOUR * FULL_DAY_HOURS}rs")
-elif checkemp == IS_PRESENT_PART_TIME:
-    print(f"Employee is present for part time and wage is {WAGE_PER_HOUR * PART_TIME_HOURS}rs")
-else:
-    print("Employee is absent")
+empAttendence = {
+    IS_PRESENT_FULL_DAY :
+    f"Employee is present for full time and the wage is {WAGE_PER_HOUR * FULL_DAY_HOURS}rs",
+    IS_PRESENT_PART_TIME:
+    f"Employee is present for part time and wage is {WAGE_PER_HOUR * PART_TIME_HOURS}rs",
+    IS_ABSENT:
+    f"Employee is absent"
+    }
+
+checkEmp = random.randint(0,2)
+print(empAttendence.get(checkEmp))
