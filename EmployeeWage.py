30b33f5f281739b40548c77adf8b4042d821ae18
TheJayesh99 <jayeshm.official@gmail.com>
2021-08-25 11:27:29 +0530

    Added : Employee Wage Builder class to store total wages of employee

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -6,23 +6,45 @@ IS_PRESENT_PART_TIME = 2
 FULL_DAY_HOURS = 8 
 PART_TIME_HOURS = 4
 
-def  calculateEmployeeWage(wagePerHour ,numberOfWorkingDays,workHrsPerMonth,company):
-    empAttendence = {
-        IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
-        IS_PRESENT_PART_TIME: PART_TIME_HOURS,
-        IS_ABSENT: 0
-        }
+class EmployeeWageBuilder():
+    def __init__(self,wage_per_hour ,number_of_working_days,work_hrs_per_month,company) -> None:
+        self.wage_per_hour = wage_per_hour
+        self.number_of_working_days = number_of_working_days
+        self.work_hrs_per_month = work_hrs_per_month
+        self.company = company
 
-    workingDays = 0
-    totalWorkingHours = 0
+    def calculate_daily_wage(self,emp_hrs):
+        return emp_hrs * self.wage_per_hour
 
-    while(workingDays < numberOfWorkingDays and totalWorkingHours < workHrsPerMonth):
-        checkEmp = random.randint(0,2)
-        totalWorkingHours = totalWorkingHours + empAttendence.get(checkEmp)
-        workingDays = workingDays + 1
+    @staticmethod
+    def check_emp_working_hours(check_emp):
+        emp_attendence = {
+            IS_PRESENT_FULL_DAY : FULL_DAY_HOURS,
+            IS_PRESENT_PART_TIME: PART_TIME_HOURS,
+            IS_ABSENT: 0
+            }
+        return emp_attendence.get(check_emp)
 
-    totalWage = totalWorkingHours * wagePerHour
-    print(f"Employee total wage for Company {company} is {totalWage}")
+    def calculate_employee_salary(self):
+        working_days = 0
+        total_working_hours = 0
+        total_wage = 0 
+        while(working_days < self.number_of_working_days and total_working_hours < self.work_hrs_per_month):
+            check_emp = random.randint(0,2)
+            emp_hrs = EmployeeWageBuilder.check_emp_working_hours(check_emp) 
+            total_working_hours += emp_hrs
+            working_days += 1
+            total_wage += self.calculate_daily_wage(emp_hrs)
+        return total_wage
+    
+    def calculate_employee_wage(self):
+        self.total_wage = self.calculate_employee_salary()
+        print(f"Employee total wage for Company {self.company} is {self.total_wage}")
 
-calculateEmployeeWage(20,20,100,"Dmart")
-calculateEmployeeWage(30,25,110,"Jio")
+
+if __name__ == "__main__":
+    print("Welcome to Employee Wage Builder")
+    emp_dmart = EmployeeWageBuilder(20,20,100,"Dmart")
+    emp_jio = EmployeeWageBuilder(30,25,110,"Jio")
+    emp_dmart.calculate_employee_wage()
+    emp_jio.calculate_employee_wage()
